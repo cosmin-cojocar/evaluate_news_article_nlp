3e1cda351fe7d4320f8b23755f2055618e03a337
Cosmin Cojocar <cmcosmin@gmail.com>
2019-09-29 14:23:16 +0300

    chore: update webpack prod file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,64 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "production",
+  stats: "verbose",
+  devServer: {
+    contentBase: "./dist"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
   module: {
     rules: [
       {
         test: "/\.js$/",
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: ["babel-loader", "eslint-loader"]
+      }, {
+        // Apply rule for .sass, .scss or .css files
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        // Set loaders to transform files.
+        // Loaders are applying from right to left(!)
+        // The first loader will be applied after others
+        use: [
+          {
+            // Creates "style" nodes from JS strings
+            loader: "style-loader"
+          }, {
+            // This loader resolves url() and @imports inside CSS
+            loader: "css-loader",
+          }, {
+            // First we transform SASS to standard CSS
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ],
       }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
+      favicon: "./src/client/favicon.ico",
       filename: "./index.html"
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
     })
   ]
 };
